c51b5acccfb5ad8c9dda797c184f56f37ba01fbd
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-11 22:57:12 +0300

    sta types

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return 
+
+    elif length == 1:
+       print([0])
+       return
+
+    elif length == 2:
+        print([0, 1])
+        return
+
+    sequence = [0, 1]
+
+    for i in range(2, length):
+        next_fib = sequence[-1] + sequence[-2]
+        sequence.append(next_fib)
+
+    print(sequence[:length])
+
+# Example usage:
+print(print_fibonacci(0))
